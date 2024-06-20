ff5ff79aa56a68894c45839318767e27bebb5a26
Akihiro Kaji <akihiro.kaji@3-shake.com>
2024-06-20 19:17:26 +0900

    nvimのconfigをluaに移行

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require 'options'
+require 'keymaps'
+require 'plugins'
+require 'colorscheme'
+require 'plugin/noice'
+require 'plugin/treesitter'
+require 'plugin/lualine'
+require 'plugin/catppuccin'
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,52 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- add any options here
+        },
+        dependencies = {
+          -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+          "MunifTanjim/nui.nvim",
+          -- OPTIONAL:
+          --   `nvim-notify` is only needed, if you want to use the notification view.
+          --   If not available, we use `mini` as the fallback
+          "rcarriga/nvim-notify",
+          }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000
+    },
+    {
+        "neanias/everforest-nvim"
+    }
+})
