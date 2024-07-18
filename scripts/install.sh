26f5b42d54462edceb6ca450f89c8684b5f0f2a1
Akihiro Kaji <safinfingg@gmail.com>
2024-07-18 22:28:28 +0900

    分岐とエラー処理を調整

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,67 +1,106 @@
 #!/usr/bin/env bash
+#
+# Install required packages and tools for dotfiles
 
-if [[ "$(uname)" == 'Darwin' ]]; then
+os="$(uname)"
+kernel="$(uname -r)"
+
+# for MacOS
+if [[ "${os}" == 'Darwin' ]]; then
 
     echo "##################################################"
-    echo "XCode Command Line Tools"
+    echo "Install XCode Command Line Tools"
     echo "##################################################"
+
     xcode-select --install
 
     echo "##################################################"
-    echo "Installing Homebrew"
+    echo "Install Homebrew"
     echo "##################################################"
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    if [[ ! -e /opt/homebrew/bin/brew ]]; then
+      NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    else
+      echo "Homebrew is already installed. skip."
+    fi
 
     echo "##################################################"
-    echo "Installing Zinit"
+    echo "Install Zinit"
     echo "##################################################"
-    bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-    sudo sed -i '/^# \/etc\/shells.*/a /opt/homebrew/bin/zsh' /etc/shells
+
+    if [[ ! -e "${HOME}/.local/share/zinit/zinit.git/zinit.zsh" ]]; then
+      bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+      sudo sed -i '/^# \/etc\/shells.*/a /opt/homebrew/bin/zsh' /etc/shells
+    else
+      echo "Zinit is already installed. skip."
+    fi
 
     echo "##################################################"
     echo "Others"
     echo "##################################################"
+
     bash ./MacOS.sh
 
-else
-  if [[ "$(uname -r)" == *microsoft* ]]; then
+elif [[ "${os}" == 'Linux' ]]; then
+  # for WSL2 Ubuntu
+  if [[ "${kernel}" == *microsoft* ]]; then
 
     echo "##################################################"
-    echo "Installing pre-requisites"
+    echo "Install pre-requisites"
     echo "##################################################"
+
     sudo add-apt-repository -y ppa:wslutilities/wslu
     sudo apt-get update && sudo apt-get install -y build-essential git unzip wget wslu
 
     echo "##################################################"
-    echo "Installing Homebrew"
+    echo "Install Homebrew"
     echo "##################################################"
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+    if [[ ! -e /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+      NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    else
+      echo "Homebrew is already installed. skip."
+    fi
 
     echo "##################################################"
-    echo "Installing Zinit"
+    echo "Install Zinit"
     echo "##################################################"
-    bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-    sudo sed -i '/^# \/etc\/shells.*/a /home/linuxbrew/.linuxbrew/bin/zsh' /etc/shells
+
+    if [[ ! -e "${HOME}/.local/share/zinit/zinit.git/zinit.zsh" ]]; then
+      bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+      sudo sed -i '/^# \/etc\/shells.*/a /home/linuxbrew/.linuxbrew/bin/zsh' /etc/shells
+    else
+      echo "Zinit is already installed. skip."
+    fi
 
     echo "##################################################"
-    echo "Installing win32yank"
+    echo "Install win32yank"
     echo "##################################################"
-    wget https://github.com/equalsraf/win32yank/releases/download/v0.1.1/win32yank-x64.zip -O /tmp/win32yank-x64.zip
+
+    wget https://github.com/equalsraf/win32yank/releases/latest/download/win32yank-x64.zip -O /tmp/win32yank-x64.zip
     unzip /tmp/win32yank-x64.zip -d /tmp
+
     if [[ ! -d "${HOME}"/bin ]]; then
       mkdir "${HOME}"/bin
     fi
+
     sudo mv /tmp/win32yank.exe "${HOME}"/bin
     sudo chmod +x "${HOME}"/bin/win32yank.exe
     rm -f /tmp/LICENSE /tmp/README.md /tmp/win32yank-x64.zip
 
     echo "##################################################"
-    echo "Installing wsl2-ssh-agent"
+    echo "Install wsl2-ssh-agent"
     echo "##################################################"
+
     wget https://github.com/mame/wsl2-ssh-agent/releases/latest/download/wsl2-ssh-agent -O "${HOME}"/bin/wsl2-ssh-agent
     chmod +x "${HOME}"/bin/wsl2-ssh-agent
+
+  else
+
+    echo "Unsupported OS. aborted."
+    exit 1
 
   fi
 fi
